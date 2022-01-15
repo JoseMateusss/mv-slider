57c32fec08d2fc787eedb5523a23096fb2ffd578
josemateusss <josemateusp08cs@gmail.com>
2022-01-15 01:39:12 -0300

    Criando classe principal e métodos de activate, deactivate e uninstall

--- mv-slider.php
+++ mv-slider.php
@@ -31,3 +31,47 @@ if( ! defined( 'ABSPATH') ){
     die("O plugin não pode ser acessado desta forma!");
     exit;
 }
+
+if( ! class_exists('MV_Slider') ){
+    
+    class MV_Slider
+    {
+        
+        function __construct()
+        {
+            $this->define_constants();
+        }
+
+        
+        public function define_constants()
+        {
+           define('MV_SLIDER_PATH', plugin_dir_path( __FILE__ )); 
+           define('MV_SLIDER_URL', plugin_dir_url( __FILE__ ));
+           define('MV_SLIDER_VERSION', '1.0.0' ));
+        }
+
+        public static function activate()
+        {
+            update_option( 'rewrite_rules', '' );
+        }
+
+        public static function deactivate()
+        {
+            flush_rewrite_rules();
+        }
+
+        public static function uninstall()
+        {
+            
+        }
+
+    }
+}
+
+if( class_exists( 'MV_Slider' ) ){
+    register_activation_hook(__FILE__, ['MV_Slider', 'activate']);
+    register_deactivation_hook(__FILE__, ['MV_Slider', 'deactivate']);
+    register_uninstall_hook(__FILE__, ['MV_Slider', 'uninstall']);
+
+    $mv_slider = new MV_Slider();
+}
