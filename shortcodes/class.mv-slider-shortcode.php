bc596c2d52160c1364b6dc6cf1709a1df8c01372
joseMateusss <josemateusp08cs@gmail.com>
2022-01-20 18:49:58 +0300

    Criando view do shortcode e loop personalizado

--- shortcodes/class.mv-slider-shortcode.php
+++ shortcodes/class.mv-slider-shortcode.php
@@ -1,7 +1,6 @@
 <?php
-
-if ( !class_exists( MV_Slider_shortcode ) ) {
-    class MV_Slider_shortcode
+if ( !class_exists( 'MV_Slider_Shortcode' ) ) {
+    class MV_Slider_Shortcode
     {
        public function __construct()
        {
@@ -23,6 +22,10 @@ if ( !class_exists( MV_Slider_shortcode ) ) {
             if( !empty( $id ) ){
                 $id = array_map( 'absint', explode( ',', $id ) );
             }
+
+            ob_start();
+            require( MV_SLIDER_PATH.'views/mv-slider_shortcode.php' );
+            return ob_get_clean();
        }
     }
     
